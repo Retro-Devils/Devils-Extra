19834a914915eb6cdc1a900e006ed2b703116594
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-09 19:57:48 -0600

    Update bbc-layout-menu.sh

--- scripts/bbc-layout-menu.sh
+++ scripts/bbc-layout-menu.sh
@@ -13,12 +13,12 @@ local choice
       2>&1 >/dev/tty)
 
     case "$choice" in
-    1)cat >"$md_inst/b-em-allegro4-multiload.sh" << _EOF_
+    1)cat >"/opt/retropie/emulators/b-em-bbc-sh/b-em-allegro4-multiload.sh" << _EOF_
       #!/bin/bash
       function load_tape() {
       cassload=();cassload=( "quotedbl" "t" "a" "p" "e" "Return" "c" "h" "a" "i" "n" "at" "at" "Return" )
       xset -dpms s off s noblank
-      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
+      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/matchbox_key_shortcuts &
       qjoypad "bbc-1" &&
       /opt/retropie/emulators/b-em-allegro4/b-em \$1 -tape "\$2"|\
       for index in \${!cassload[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${cassload[\$index]} sleep 0.1 keyup \${cassload[\$index]};done
@@ -26,19 +26,19 @@ local choice
       function load_disc() {
       #dfs autoload with Shift_L+F12
       xset -dpms s off s noblank
-      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
+      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc-sh/matchbox_key_shortcuts &
       qjoypad "bbc-1" &&
       /opt/retropie/emulators/b-em-allegro4/b-em \$1 -disc "\$2" | xdotool sleep 1.5 keydown Shift_L+F12 sleep 1 keyup Shift_L+F12
       }
       [[ "\$2" == *.uef ]] && load_tape \$1 "\$2"
       [[ "\$2" == *.ssd ]] && load_disc \$1 "\$2"
       _EOF_ ;;
-    2)cat >"$md_inst/b-em-allegro4-multiload.sh" << _EOF_
+    2)cat >"/opt/retropie/emulators/b-em-bbc-sh/b-em-allegro4-multiload.sh" << _EOF_
       #!/bin/bash
       function load_tape() {
       cassload=();cassload=( "quotedbl" "t" "a" "p" "e" "Return" "c" "h" "a" "i" "n" "at" "at" "Return" )
       xset -dpms s off s noblank
-      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
+      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc-sh/matchbox_key_shortcuts &
       qjoypad "bbc-2" &&
       /opt/retropie/emulators/b-em-allegro4/b-em \$1 -tape "\$2"|\
       for index in \${!cassload[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${cassload[\$index]} sleep 0.1 keyup \${cassload[\$index]};done
@@ -46,19 +46,19 @@ local choice
       function load_disc() {
       #dfs autoload with Shift_L+F12
       xset -dpms s off s noblank
-      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
+      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc-sh/matchbox_key_shortcuts &
       qjoypad "bbc-2" &&
       /opt/retropie/emulators/b-em-allegro4/b-em \$1 -disc "\$2" | xdotool sleep 1.5 keydown Shift_L+F12 sleep 1 keyup Shift_L+F12
       }
       [[ "\$2" == *.uef ]] && load_tape \$1 "\$2"
       [[ "\$2" == *.ssd ]] && load_disc \$1 "\$2"
       _EOF_ ;;
-    2)cat >"$md_inst/b-em-allegro4-multiload.sh" << _EOF_
+    2)cat >"/opt/retropie/emulators/b-em-bbc-sh/b-em-allegro4-multiload.sh" << _EOF_
       #!/bin/bash
       function load_tape() {
       cassload=();cassload=( "quotedbl" "t" "a" "p" "e" "Return" "c" "h" "a" "i" "n" "at" "at" "Return" )
       xset -dpms s off s noblank
-      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
+      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc-sh/matchbox_key_shortcuts &
       qjoypad "bbc-3" &&
       /opt/retropie/emulators/b-em-allegro4/b-em \$1 -tape "\$2"|\
       for index in \${!cassload[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${cassload[\$index]} sleep 0.1 keyup \${cassload[\$index]};done
@@ -66,19 +66,19 @@ local choice
       function load_disc() {
       #dfs autoload with Shift_L+F12
       xset -dpms s off s noblank
-      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
+      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc-sh/matchbox_key_shortcuts &
       qjoypad "bbc-3" &&
       /opt/retropie/emulators/b-em-allegro4/b-em \$1 -disc "\$2" | xdotool sleep 1.5 keydown Shift_L+F12 sleep 1 keyup Shift_L+F12
       }
       [[ "\$2" == *.uef ]] && load_tape \$1 "\$2"
       [[ "\$2" == *.ssd ]] && load_disc \$1 "\$2"
       _EOF_ ;;
-    4)cat >"$md_inst/b-em-allegro4-multiload.sh" << _EOF_
+    4)cat >"/opt/retropie/emulators/b-em-bbc-sh/b-em-allegro4-multiload.sh" << _EOF_
       #!/bin/bash
       function load_tape() {
       cassload=();cassload=( "quotedbl" "t" "a" "p" "e" "Return" "c" "h" "a" "i" "n" "at" "at" "Return" )
       xset -dpms s off s noblank
-      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
+      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc-sh/matchbox_key_shortcuts &
       qjoypad "bbc-4" &&
       /opt/retropie/emulators/b-em-allegro4/b-em \$1 -tape "\$2"|\
       for index in \${!cassload[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${cassload[\$index]} sleep 0.1 keyup \${cassload[\$index]};done
@@ -86,7 +86,7 @@ local choice
       function load_disc() {
       #dfs autoload with Shift_L+F12
       xset -dpms s off s noblank
-      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
+      matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc-sh/matchbox_key_shortcuts &
       qjoypad "bbc-4" &&
       /opt/retropie/emulators/b-em-allegro4/b-em \$1 -disc "\$2" | xdotool sleep 1.5 keydown Shift_L+F12 sleep 1 keyup Shift_L+F12
       }
