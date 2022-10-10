b02b9f92017516da74276ecbea7b355205f6b2fd
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-09 21:32:00 -0600

    Update bbc-layout-menu.sh

--- scripts/bbc-layout-menu.sh
+++ scripts/bbc-layout-menu.sh
@@ -6,10 +6,10 @@ local choice
     choice=$(dialog --backtitle "$BACKTITLE" --title "BBC LAYOUT MENU  " \
      --ok-button Select --cancel-button Back \
      --menu "CHOOSE A LAYOUT " 30 70 50 \
-      1 "LAYOUT 1:  1,2,3,4,5,6--DPAD " \
-      2 "LAYOUT 2:  W,A,S,D--" \
-      3 "LAYOUT 3: " \
-      4 "LAYOUT 4: " \
+      1 "LAYOUT 1: bbc-1 " \
+      2 "LAYOUT 2: bbc-2 " \
+      3 "LAYOUT 3: bbc-3 " \
+      4 "LAYOUT 4: bbc-4 " \
       2>&1 >/dev/tty)
 
     case "$choice" in
@@ -61,6 +61,52 @@ function load_disc() {
 xset -dpms s off s noblank
 matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc/matchbox_key_shortcuts 
 qjoypad "bbc-2" &&
+/opt/retropie/emulators/b-em-bbc/b-em \$1 -disc "\$2" | xdotool sleep 1.5 keydown Shift_L+F12 sleep 1 keyup Shift_L+F12
+}
+[[ "\$2" == *.uef ]] && load_tape \$1 "\$2"
+[[ "\$2" == *.ssd ]] && load_disc \$1 "\$2"
+_EOF_
+}
+
+function layout3() {
+    cat >"/opt/retropie/emulators/b-em-bbc/b-em-allegro4-multiload.sh" << _EOF_
+#!/bin/bash
+function load_tape() {
+cassload=();cassload=( "quotedbl" "t" "a" "p" "e" "Return" "c" "h" "a" "i" "n" "at" "at" "Return" )
+xset -dpms s off s noblank
+matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc/matchbox_key_shortcuts &
+qjoypad "bbc-3" &&
+/opt/retropie/emulators/b-em-bbc/b-em \$1 -tape "\$2"|\
+for index in \${!cassload[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${cassload[\$index]} sleep 0.1 keyup \${cassload[\$index]};done
+}
+function load_disc() {
+#dfs autoload with Shift_L+F12
+xset -dpms s off s noblank
+matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc/matchbox_key_shortcuts 
+qjoypad "bbc-3" &&
+/opt/retropie/emulators/b-em-bbc/b-em \$1 -disc "\$2" | xdotool sleep 1.5 keydown Shift_L+F12 sleep 1 keyup Shift_L+F12
+}
+[[ "\$2" == *.uef ]] && load_tape \$1 "\$2"
+[[ "\$2" == *.ssd ]] && load_disc \$1 "\$2"
+_EOF_
+}
+
+function layout4() {
+    cat >"/opt/retropie/emulators/b-em-bbc/b-em-allegro4-multiload.sh" << _EOF_
+#!/bin/bash
+function load_tape() {
+cassload=();cassload=( "quotedbl" "t" "a" "p" "e" "Return" "c" "h" "a" "i" "n" "at" "at" "Return" )
+xset -dpms s off s noblank
+matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc/matchbox_key_shortcuts &
+qjoypad "bbc-4" &&
+/opt/retropie/emulators/b-em-bbc/b-em \$1 -tape "\$2"|\
+for index in \${!cassload[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${cassload[\$index]} sleep 0.1 keyup \${cassload[\$index]};done
+}
+function load_disc() {
+#dfs autoload with Shift_L+F12
+xset -dpms s off s noblank
+matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig /opt/retropie/emulators/b-em-bbc/matchbox_key_shortcuts 
+qjoypad "bbc-4" &&
 /opt/retropie/emulators/b-em-bbc/b-em \$1 -disc "\$2" | xdotool sleep 1.5 keydown Shift_L+F12 sleep 1 keyup Shift_L+F12
 }
 [[ "\$2" == *.uef ]] && load_tape \$1 "\$2"
