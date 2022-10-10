941fb951bf9d824d978c0c605e6fe98384d496dc
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-09 21:43:09 -0600

    Update bbc-layout-menu.sh

--- scripts/bbc-layout-menu.sh
+++ scripts/bbc-layout-menu.sh
@@ -10,6 +10,8 @@ local choice
       2 "LAYOUT 2: bbc-2 " \
       3 "LAYOUT 3: bbc-3 " \
       4 "LAYOUT 4: bbc-4 " \
+      5 "LAYOUT 5: bbc-5 " \
+      6 "LAYOUT MENU HELP"
       2>&1 >/dev/tty)
 
     case "$choice" in
@@ -17,6 +19,8 @@ local choice
     2) layout2 ;;
     3) layout3 ;;
     4) layout4 ;;
+    5) layout5 ;;
+    6) layout-help ;;
     *) break ;;
     esac
   done
@@ -113,5 +117,25 @@ qjoypad "bbc-4" &&
 [[ "\$2" == *.ssd ]] && load_disc \$1 "\$2"
 _EOF_
 }
+
+function layout-help() {
+dialog  --sleep 1 --title "BBC LAYOUT HELP" --msgbox " 
+WHY EVEN MAKE THIS?
+     - We want everyone to enjoy alL the games.
+HOWS THIS WORK?
+     - This edits BBC emu launch commands to load different qjoypad layouts.
+     - Each layout has the name of layout next to it. Example bbc-1
+HOW DO I USE IT ?
+     - Open pixel desktop
+     - Open qjoypad
+     - Click qjoypad icon in tray.
+     - Add a new layout.
+     - Name bbc-1, bbc-2, bbc-3 or bbc-4.
+     - Map your buttons to keyboard .
+     - Save your new layout .
+     - Enjoy!
+WHERE DID THIS COME FROM?
+     - We Retro Devils made this from stratch to enjoy BBC." 0 0
+     
 
 bbc-controller-layout
