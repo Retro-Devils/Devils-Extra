87b8c8da671c1406a3c4f3eaa71adc2f6889e7e3
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-25 18:27:34 -0600

    Update bbc-installer.sh

--- scripts/bbc-installer.sh
+++ scripts/bbc-installer.sh
@@ -1,16 +1,50 @@
 #!/bin/bash
 export NCURSES_NO_UTF8_ACS=1
+#!/bin/bash
 
-echo "BBC INSTALLER WILL NOW BEGIN"
-sleep 5
-echo "IF YOU WANNA CANCEL PLEASE PRESS CONTROL+C NOW"
-sleep 5
+export NCURSES_NO_UTF8_ACS=1
+
+###----------------------------###
+### INSTALLER MENU FUNCTIONS   ###
+###----------------------------###
+function bbc-install-menu() {
+    local choice
+
+    while true; do
+        choice=$(dialog --backtitle "$BACKTITLE" --title "BBC MICRO INSTALLER V1.1 " \
+            --ok-label Select --cancel-label Exit-Installer \
+            --menu "Pi.D.E.I INSTALLER" 25 50 30 \
+            1 "Install/Update BBC Micro " \
+            2 "BBC Micro Information   " \
+            - "__________        __                  " \
+            - "\______   \ _____/  |________  ____   " \
+            - " |       _// __ \   __\_  __ \/  _ \  " \
+            - " |    |   \  ___/|  |  |  | \(  <_> ) " \
+            - " |____|_  /\___  >__|  |__|   \____/  " \
+            - "        \/     \/                     " \
+            - "________              .__.__          " \
+            - "\______ \   _______  _|__|  |   ______" \
+            - " |    |  \_/ __ \  \/ /  |  |  /  ___/" \
+            - " |    '   \  ___/\   /|  |  |__\___ \ " \
+            - "/_______  /\___  >\_/ |__|____/____  >" \
+            - "        \/     \/                  \/ " \
+            2>&1 > /dev/tty)
+
+        case "$choice" in
+            1) install-bbc  ;;
+            2) bbc-info     ;;
+            -) no ;;
+	          +) none ;;
+            *) break       ;;
+        esac
+    done
+}
+
+function install-bbc() {
 wget https://raw.githubusercontent.com/Retro-Devils/Devils-Extra/main/scriptmodules/emulators/b-em-2.2.sh -P $HOME/RetroPie-Setup/scriptmodules/emulators/
 sleep 3
-echo "INSTALLER WILL NOW INSTALL B-EM 2.2 EMULATOR"
 sudo $HOME/RetroPie-Setup//retropie_packages.sh b-em-2-2
 sleep 2
-echo "INSTALLER WILL NOW GRAB A BLANK QJOYPAD LAYOUT NAMED bbcmicro "
 wget https://raw.githubusercontent.com/Retro-Devils/Devils-Extra/main/scripts/bbcmicro.lyt -P $HOME/.qjoypad3/
 sleep 2
 dialog  --sleep 1 --title "BBC INSTALLER INFO" --msgbox " 
@@ -29,3 +63,8 @@ dialog  --sleep 1 --title "BBC INSTALLER INFO" --msgbox "
 THANKS FOR INSTALLING
 REPORT ERRORS TO DEVILS
 PLEASE ENJOY" 0 0
+}
+
+function bbc-info() {
+echo "blah blah"
+}
