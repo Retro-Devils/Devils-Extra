efeb9b28113b57d894a8033765dd69ac39226191
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-11-13 20:26:36 -0700

    Delete files/emu.cfg directory

--- files/emu.cfg/wine/Spooky-Castle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/pi/RetroPie/roms/wine/games/.installs/Spooky-Castle/
-WINEDEBUG=-all LD_LIBRARY_PATH="/opt/retropie/supplementary/mesa/lib/" setarch linux32 -L /opt/retropie/emulators/wine/bin/wine "/home/pi/RetroPie/roms/wine/games/.installs/Spooky-Castle/Spooky_Castle.exe"
